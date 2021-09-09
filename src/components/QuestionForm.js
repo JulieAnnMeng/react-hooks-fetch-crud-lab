1b4b191b502eb2ba2b292fe267fa20dbce636b4b
Julie Meng <julieannmeng@gmail.com>
2021-09-09 11:51:50 -0400

    commit tree f102f2c14612879c04b0f7ce81dd871c9d4e6233

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
-  const [formData, setFormData] = useState({
+function QuestionForm({setUpdate}) {
+  const blankForm = {
     prompt: "",
     answer1: "",
     answer2: "",
     answer3: "",
     answer4: "",
     correctIndex: 0,
-  });
+  }
+  const [formData, setFormData] = useState(blankForm);
 
   function handleChange(event) {
     setFormData({
@@ -19,7 +20,22 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify ({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1, 
+          formData.answer2, 
+          formData.answer3, 
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    })
+    setFormData(blankForm)
+    setUpdate(true)
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,41 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, setUpdate }) {
+
+  const questionList = questions.map(question => {
+    return(
+      <QuestionItem 
+        key={question.id}
+        question={question}
+        handleDelete={handleDelete}
+        handlePatch={handlePatch}
+      />
+    )
+  })
+
+  function handleDelete(question) {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: 'DELETE',
+    })
+    setUpdate(true)
+  }
+
+  function handlePatch(e, question) {
+    const newCorrectIndex = parseInt(e.target.selectedIndex);
+    debugger;
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: 'PATCH',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify ({"correctIndex": newCorrectIndex})
+    })
+    setUpdate(true)
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList}</ul>
     </section>
   );
 }
